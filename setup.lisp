239d621b730bc55904ba3a89f1e5ff17d2c3fab4
Heitor Chang <heitorchang@gmail.com>
2016-06-10 17:05:47 -0300

    clisp cd

--- setup.lisp
+++ setup.lisp
@@ -1,2 +1,5 @@
+;;;; CLISP
+(ext:cd "C:\\Users\\Heitor\\Desktop\\emacs-24.3\\bin\\gigamonkeys") 
+
 ;;;; Clozure CL
-(:cd "C:\\Users\\Heitor\\Desktop\\emacs-24.3\\bin\\gigamonkeys")
+;;; (:cd "C:\\Users\\Heitor\\Desktop\\emacs-24.3\\bin\\gigamonkeys")
